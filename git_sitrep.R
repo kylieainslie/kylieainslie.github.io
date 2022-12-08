480dd76ccca30f012f467a7b54a744e9fd2c2b0e
Kylie Ainslie <kylieainslie@Kylies-MacBook-Pro.local>
2022-12-08 20:44:05 +0100

    git sitrep file

---
+++ git_sitrep.R
@@ -0,0 +1,15 @@
+options(
+  usethis.full_name = "Kylie Ainslie",
+  usethis.protocol  = "ssh",
+  usethis.description = list(
+    "Authors@R" = utils::person(
+      "Kylie", "Ainslie",
+      email = "ainslie.kylie@gmail.com",
+      role = c("aut", "cre"),
+      comment = c(ORCID = "0000-0001-5419-7206")
+    ),
+    Version = "0.0.0.9000"
+  ),
+  usethis.destdir = "~/Documents",
+  usethis.overwrite = TRUE
+)
